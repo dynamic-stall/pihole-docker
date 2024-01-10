7ccf813f52c0c4aff2b7240e0cf24a8f2e183376
dynamic-stall <76631795+dynamic-stall@users.noreply.github.com>
2024-01-10 13:31:35 -0700

    Update custom-network.sh

--- custom-network.sh
+++ custom-network.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Variables:
+NETWORK_NAME=law-net
+SUBNET=172.31.10.0/24
+GATEWAY=172.31.10.1
 
 docker network create \
 --driver=bridge \
 --attachable \
---subnet=172.31.10.0/24 \
---gateway=172.31.10.1 \
-law-net
+--subnet=$SUBNET \
+--gateway=$GATEWAY \
+$NETWORK_NAME
